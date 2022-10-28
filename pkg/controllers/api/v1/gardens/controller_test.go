c238704c6ccfb7cd21b79b8c9915e117667008e3
uriel robles <urieleduardo.roblesnava@constantcontact.com>
2022-10-28 01:25:09 -0500

    complete crud test

--- pkg/controllers/api/v1/gardens/controller_test.go
+++ pkg/controllers/api/v1/gardens/controller_test.go
@@ -1,6 +1,8 @@
 package gardens
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -50,4 +52,47 @@ func TestGetGardenSuccessfully(t *testing.T) {
 	r.ServeHTTP(w, req)
 	assert.NotNil(t, entity)
 	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestUpdateGardenSuccessfully(t *testing.T) {
+	mock.SetUpRouter()
+	registry := repository.NewRepositoryRegistry(mock.DB, &repository.GardenRepository{})
+	r := mock.Router
+	garden := models.Garden{Name: "Uriel"}
+	body := models.Garden{Name: "Uriel Update"}
+
+	r.PATCH("/gardens/:GardenId", UpdateGarden)
+
+	w := httptest.NewRecorder()
+
+	ctx, _ := gin.CreateTestContext(w)
+	ctx.Set("RepositoryRegistry", registry)
+	createdGarden, _ := GetGardenRepository(ctx).Create(&garden)
+
+	jsonValue, _ := json.Marshal(body)
+	req, _ := http.NewRequest("PATCH", fmt.Sprintf("/gardens/%d", createdGarden.(*models.Garden).ID), bytes.NewBuffer(jsonValue))
+	r.ServeHTTP(w, req)
+	if err := json.Unmarshal(w.Body.Bytes(), &garden); err != nil { // Parse []byte to go struct pointer
+		fmt.Println("Can not unmarshal JSON")
+	}
+	assert.Equal(t, garden.Name, body.Name)
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestDeleteGardenSuccessfully(t *testing.T) {
+	mock.SetUpRouter()
+	r := mock.Router
+	body := models.Garden{Name: "Uriel"}
+
+	r.DELETE("/gardens/:GardenId", DeleteGarden)
+
+	w := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(w)
+	registry := repository.NewRepositoryRegistry(mock.DB, &repository.GardenRepository{})
+	ctx.Set("RepositoryRegistry", registry)
+	entity, _ := GetGardenRepository(ctx).Create(&body)
+
+	req, _ := http.NewRequest("DELETE", fmt.Sprintf("/gardens/%d", entity.(*models.Garden).ID), nil)
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusNoContent, w.Code)
 }
